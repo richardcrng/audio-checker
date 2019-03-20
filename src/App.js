b8251494e84521eb4e079795e055501b6fd8fb36
Richard Ng <richardcrng@gmail.com>
2019-03-20 10:09:54 +0000

    install lodash and react-use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+import { audios } from './audio/audio.json'
 
 function App(props) {
   return (
