3753e4fc0b688303aee8410d04dacd56df7b0ffc
Richard Ng <richardcrng@gmail.com>
2019-04-03 12:15:27 +0100

    prettify downloaded output json

--- src/App.js
+++ src/App.js
@@ -32,10 +32,8 @@ function App(props) {
   )
 }
 
-
-
 function downloadObjectAsJson(exportObj, exportName) {
-  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj, null, 2));
   var downloadAnchorNode = document.createElement('a');
   downloadAnchorNode.setAttribute("href", dataStr);
   downloadAnchorNode.setAttribute("download", exportName + ".json");
