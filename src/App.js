1986e898a6d8686704025310103210bac9bbc2c1
Richard Ng <richardcrng@gmail.com>
2019-04-03 12:05:35 +0100

    save audio durations to localStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import _ from 'lodash';
 import React from 'react';
 import './App.css';
 
-import { audios } from './audio/audio.json'
+import audios from './audio/input.json'
 import Audio from './audio/Audio';
+import { useLocalStorage } from 'react-use';
 
 function App(props) {
+  const [output, setOutput] = useLocalStorage('output', {})
+
+  React.useEffect(() => {
+    console.log("output", output)
+  }, [output])
+
   return (
     <div>
-      {audios.map(base64 => (
+      {_.map(audios, (base64, strKey) => (
         <Audio
+          key={strKey}
+          strKey={strKey}
           base64={base64}
-          key={base64}
+          output={output}
+          setOutput={setOutput}
         />
       ))}
     </div>
--- src/audio/Audio.js
+++ src/audio/Audio.js
@@ -1,16 +1,31 @@
+import _ from 'lodash';
 import React from 'react';
-import { useAudio } from 'react-use';
+import { useAudio, useLocalStorage } from 'react-use';
 import AudioButton from './button/AudioButton';
 import { MdFastRewind, MdFastForward } from 'react-icons/md';
 
 function Audio(props) {
+
   const [audio, state, controls, ref] = useAudio({
     src: `data:audio/wav;base64,${props.base64}`
   })
 
+  React.useEffect(() => {
+    if (_.get(props.output, props.strKey) != _.get(state, 'duration') && _.get(state, 'duration') != 0) {
+      props.setOutput(prevOutput => {
+        console.log("prevOutput", prevOutput)
+        return {
+          ...prevOutput,
+          [props.strKey]: _.get(state, 'duration')
+        }
+      })
+    }
+  }, [props.output, state.duration])
+
   return (
     <div>
       {audio}
+      <h4>{props.strKey}</h4>
       <pre>{JSON.stringify(state, null, 2)}</pre>
       <MdFastRewind size={40} onClick={() => controls.seek(state.time - 5)} />
       <AudioButton
